8aadcd2c5bcb0e006118ac774da498bf0765c23d
stephanie <steumutoni@gmail.com>
2022-07-01 07:55:06 -0700

    Cprogram that prints to standard output using puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ *main function used to print to standard output using puts() function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
