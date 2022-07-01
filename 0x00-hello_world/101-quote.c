c7f41dc808800076e9ee428894175bac2c0d17c2
stephanie <steumutoni@gmail.com>
2022-07-01 10:26:21 -0700

    Cprogram that prints to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char str[120] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[120] = "and that piece of art is useful\" - Dora Korpar, 2015-1					0-19\n";
 
 	fwrite(str, sizeof(str), 1, stderr);
 	return (1);
