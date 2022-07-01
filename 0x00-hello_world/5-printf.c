933ae660c2168a002f40293caa92f80856a38a0c
stephanie <steumutoni@gmail.com>
2022-07-01 08:13:59 -0700

    Cprogram that print to standard output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - displays to standard output using printf function
+ *
+ * Return: 0 to signify success
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
