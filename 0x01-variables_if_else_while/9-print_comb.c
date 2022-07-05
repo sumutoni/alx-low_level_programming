558b129efb2bc8c098ad1cbeac4ddd74e56336d6
stephanie <steumutoni@gmail.com>
2022-07-05 14:38:34 -0700

    program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible combo of single-digit values
+ *
+ * Return: 0 signify success
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
