0759702359021cf2a679b781597a68125c68cfeb
stephanie <steumutoni@gmail.com>
2022-07-05 09:51:59 -0700

    program to print all digits from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print digits from 0 
+ *
+ * Return: 0 signify success
+ */
+int main(void)
+{
+	int num=0;
+
+	while (num < 10)
+	{
+		printf(num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
