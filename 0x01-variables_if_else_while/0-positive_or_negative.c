c764c2c5fd3a0f2fb84d90b04a699a30526cbb15
stephanie <steumutoni@gmail.com>
2022-07-05 08:00:26 -0700

    program that determines if a number is postive, negative, or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /* more headers goes there */
 
 
@@ -20,11 +20,11 @@ int main(void)
 				
 	/* your code goes there */
 	if (n<0)
-		printf ("%d is negative", &n);
+		printf ("%d is negative", n);
 	else if (n>0)
-		printf ("%d is positive", &n);
+		printf ("%d is positive", n);
 	else
-		printf ("%d is zero", &n);
+		printf ("%d is zero", n);
 	
 	return (0);
 
