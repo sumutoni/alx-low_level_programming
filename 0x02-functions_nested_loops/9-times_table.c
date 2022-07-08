2f83d95562fc8c6e773c4d013a3b40afec61b8b3
stephanie <steumutoni@gmail.com>
2022-07-08 10:51:57 -0700

    program that prints times table up to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int i, j;
+	int prod;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			prod = i * j;
+			if (i != 9 && j != 9)
+			{
+				if (prod < 10)
+				{
+					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				if (prod < 10)
+				{
+					_putchar((prod % 10) + '0');
+				}
+				else
+				{
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
+			}
+		}
+	}
+}
