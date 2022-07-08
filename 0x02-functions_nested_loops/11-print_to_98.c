17b27e37340046b59e9a9e72592cae88e501c83d
stephanie <steumutoni@gmail.com>
2022-07-08 10:10:30 -0700

    program that prints numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,29 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
-		printf("%d", n);
-		n++;
+		if(n == 98)
+		{
+			printf("%d", n);
+			break;
+		}
+		else
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
 	while (n >= 98)
 	{
-		printf("%d", n);
-		n--;
+		if (n == 98)
+		{
+			printf("%d", n);
+			break;
+		}
+		else
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
+	printf("\n");
 }
