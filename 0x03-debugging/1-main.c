8f81bced04493dec204cb56f3084ae5771de4e38
stephanie <steumutoni@gmail.com>
2022-07-09 13:37:31 -0700

    program to test infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
 
@@ -10,21 +9,16 @@ int main(void)
 {
 	int i;
 
-
 	printf("Infinite loop incoming :(\n");
 
-
 	i = 0;
-
 	/**
 	while (i < 10)
 	{
 		putchar(i);									        }
 	}
 	*/
-	
 	printf("Infinite loop avoided! \\o/\n");
-
 
 	return (0);
 }
