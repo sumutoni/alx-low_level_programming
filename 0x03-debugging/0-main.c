8859d64fa228f3a4c5496925fb1afb9b89f56f40
stephanie <steumutoni@gmail.com>
2022-07-09 13:14:28 -0700

    program to test positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function to test function that prints if int is positive or negative
+ * 
+ * Return: 0 signify success
+ */
+int main(void)
+{
+	int i = 90;
+
+	positive_or_negative(i);
+	return (0);
+}
