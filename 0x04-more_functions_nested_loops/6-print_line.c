81046eaee72b1ab5e544f4f71351ebf87689d103
stephanie <steumutoni@gmail.com>
2022-07-11 14:37:28 -0700

    program to print _ n number of times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print _ n number of times
+ */
+void print_line(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
