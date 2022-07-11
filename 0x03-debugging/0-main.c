423cf412fd5bb5faad967137346c029108a8cb57
stephanie <steumutoni@gmail.com>
2022-07-11 03:09:28 -0700

    program to check if a number is positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,12 @@
 #include "main.h"
 
 /**
- * main - function to test function that prints if int is positive or negative
- * 
+ * main - function to test function that prints if int is positive or negative 
  * Return: 0 signify success
  */
 int main(void)
 {
-	int i = 90;
+	int i = 0;
 
 	positive_or_negative(i);
 	return (0);
