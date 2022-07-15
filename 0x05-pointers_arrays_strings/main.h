817baa9d879e73ac56a961b5e1d4134174dd3563
stephanie <steumutoni@gmail.com>
2022-07-15 12:50:10 -0700

    Header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,8 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *s);
+void puts_half(char *str);
+void print_arrary(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
