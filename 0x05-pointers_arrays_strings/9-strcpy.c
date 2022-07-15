c5b30a7225fa56038c0ecf8d8ddbca73ca7fba61
stephanie <steumutoni@gmail.com>
2022-07-15 12:52:49 -0700

    program to copy a string into another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * _strcpy - copy string into another
  * @dest: destination string
  * @src: source string
  */
-char *_strcpy(char *dest, char &src)
+char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
 	return (dest);
