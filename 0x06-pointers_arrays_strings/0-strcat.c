a75a65869cfd9a21008be6c5f4ad20e6c95e51b4
stephanie <steumutoni@gmail.com>
2022-07-18 03:40:17 -0700

    program to concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,6 +5,8 @@
  * _strcat - function to concatenate two strings
  * @dest: destination string
  * @src: source string
+ *
+ * Return: pointer to string
  */
 char *_strcat(char *dest, char *src)
 {
