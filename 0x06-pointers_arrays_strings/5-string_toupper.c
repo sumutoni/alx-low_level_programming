e6154aa812737d9d1ea7675f638d52aa85362637
stephanie <steumutoni@gmail.com>
2022-07-18 07:40:16 -0700

    program to change to uppercase lowercase characters

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * string_toupper - function to change characters from lowercase to uppercase
+ * @s: string
+ *
+ * Return: pointer to string
+ */
+char *string_toupper(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		if (isupper(s[i]) == 0)
+		{
+			toupper(s[i]);
+		}
+	}
+	return (s);
+}
