41d257b85df87a71a54b19c770859f0f5b9eb8ac
stephanie <steumutoni@gmail.com>
2022-07-19 12:05:22 -0700

    function to return a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - returns first occurence of a character in a string
+ * @s: string to search in
+ * @c: character to find
+ *
+ * Return: pointer to the first occurence of character
+ */
+char *_strchr(char *s, char c)
+{
+	int i, len;
+	char *ptr;
+
+	//strchr(s, c);
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+	}
+	return (ptr);
+}
