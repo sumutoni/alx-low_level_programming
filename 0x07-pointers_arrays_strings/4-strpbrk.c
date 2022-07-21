07b82dddec318110b429713bd286295934486c03
stephanie <steumutoni@gmail.com>
2022-07-21 09:23:11 -0700

    function that finds the first character in the string s1 that matches any character specified in s2

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 /**
- * _strprbk - function finds the first character in the 
+ * _strpbrk - function finds the first character in the
  * string s1 that matches any character specified in s2
  * (It excludes terminating null-characters)
  * @s: string to scan
  * @accept: string with characters to match
  *
- * Return: a pointer to the character in s1 that 
+ * Return: a pointer to the character in s1 that
  * matches one of the characters in s2, else returns NULL.
  */
 char *_strpbrk(char *s, char *accept)
