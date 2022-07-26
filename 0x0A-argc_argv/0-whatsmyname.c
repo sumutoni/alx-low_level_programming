cb29144f65f552663a65e0fa2ad201768a6258af
stephanie <steumutoni@gmail.com>
2022-07-26 13:54:26 -0700

    program to print its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  *
  * Return: 0 for success
  */
-int main(int __attribute__((unused)) argc, char* argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
