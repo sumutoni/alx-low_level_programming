b62584348510997c772a3630794be149dcfd6746
stephanie <steumutoni@gmail.com>
2022-07-26 09:17:27 -0700

    program to print its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program to print its name
+ * @argc: number of arguments passed
+ * @argc: array of strings to hold arguments passed
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory is for programs working with argc and argv in C
