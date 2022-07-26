b85554ed445ce250a0b30f05b3be59398c1edc2b
stephanie <steumutoni@gmail.com>
2022-07-26 09:24:06 -0700

    program to print its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
 /**
  * main - program to print its name
  * @argc: number of arguments passed
- * @argc: array of strings to hold arguments passed
+ * @argv: array of strings to hold arguments passed
  *
  * Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
