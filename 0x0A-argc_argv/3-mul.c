b0aca09d182e3b758401b567c45aaf31e8118384
stephanie <steumutoni@gmail.com>
2022-07-26 13:40:35 -0700

    program to multiply 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: number of arguments
+ * @argv: string array of arguments
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i, num;
+
+	num = 1;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			num *= atoi(argv[i]);
+		}
+		printf("%d\n", num);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
