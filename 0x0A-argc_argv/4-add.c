ecac9c89c0ceddbc69234db84dfe19115df79607
stephanie <steumutoni@gmail.com>
2022-07-27 07:47:00 -0700

    program to add 2 numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,6 +4,19 @@
 #include "main.h"
 
 /**
+ * check_symbol - checks if there is a non digit symbol in string
+ * @s: string
+ *
+ * Return: 1 if there is 0 if not
+ */
+int check_symbol(char *s)
+{
+	if (atoi(s) == 0 && s != "0")
+		return (0);
+	else
+		return (1);
+}
+/**
  * main - adds 2 numbers
  * @argc: number of arguments
  * @argv: string array of arguments
@@ -12,14 +25,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, num;
+	int i, num, check;
 
 	num = 0;
 	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			num += atoi(argv[i]);
+			check = check_symbol(argv[i]);
+			if (check == 1)
+				num += atoi(argv[i]);
+			else
+			{
+				print("Error\n");
+				return (1);
+			}
 		}
 		printf("%d\n", num);
 	}
