a89aa92baa1271c69aa3eb214d50e91ad4bfc221
stephanie <steumutoni@gmail.com>
2022-07-31 14:49:53 -0700

    function to free a 2D array of integers

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_grid: function free 2D array of ints
+ * @grid: array of ints
+ * @height: numbers of rows
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i < height; i++)
+	{
+		if (grid[i] != NULL)
+			free(grid[i]);
+		else
+			break;
+	}
+}
