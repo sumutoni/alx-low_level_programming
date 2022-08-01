125ec6e1e9505136b47a0fa97c80e0c31e197e0c
stephanie <steumutoni@gmail.com>
2022-08-01 10:43:09 -0700

    function to multiply 2 integers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * test_int - function to test if input is an integer
+ * @value: value to test
+ *
+ * Return: 1 if yes, 0 if no
+ */
+int test_int(char *value)
+{
+	int res;
+
+	res = atoi(value);
+
+	if (res == 0 && value != "0")
+		return 0;
+	else
+		return 1;
+}
+/**
+ * main - receives 2 integers and multiply them
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i, res, product;
+
+	product = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		res = test_int(argv[i]);
+		if (res == 1)
+		{
+			product *= atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			exit (98);
+		}
+	}
+	printf("%d\n", product);
+	return (0);
+}
