3669bc1f33d5fb6d388c3e9166e507f6289e4844
stephanie <steumutoni@gmail.com>
2022-08-01 07:55:23 -0700

    function to concatenate 2 strings based on bytes of second string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,47 @@
 #include <stdlib.h>
 
 /**
+ * concat - concatenates strings based on number of
+ * bytes of second string
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes
+ * @cpy: pointer to concatenated string
+ * @len1: length of s1
+ * @len2: length of s2
+ *
+ * Return: pointer to concatenated string
+ */
+char *concat(char *s1, char *s2, char *cpy, unsigned int n, int len1, int len2)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; i < len1; i++)
+	{
+		cpy[i] = s1[i];
+		j++;
+	}
+	if ((int)n > len2)
+	{
+		for (i = 0; i < len2; i++)
+		{
+			cpy[i] = s2[i];
+			j++;
+		}
+	}
+	else
+	{
+		for (i = 0; i < (int)n; i++)
+		{
+			cpy[i] = s2[i];
+			j++;
+		}
+	}
+	cpy[++j] = '\0';
+	return (cpy);
+}
+/**
  * str_nconcat - concatenates n bytes of second string to first string
  * @s1: first string
  * @s2: second string
@@ -13,7 +54,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *cpy;
-	int len1, len2, total_len, i, j;
+	int len1, len2, total_len;
 
 	if (s1 == NULL || s2 == NULL)
 	{
@@ -28,29 +69,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	cpy = (char *)malloc((total_len + 1) * sizeof(char));
 	if (cpy == NULL)
 		return (NULL);
-	j = 0;
-	for (i = 0; i < len1; i++)
-	{
-		cpy[i] = s1[i];
-		j++;
-	}
-	if ((int)n > len2)
-	{
-		for (i = 0; i < len2; i++)
-		{
-			cpy[j] = s2[i];
-			j++;
-		}
-	}
-	else
-	{
-		for (i = 0; i < (int)n; i++)
-		{
-			cpy[j] = s2[i];
-			j++;
-		}
-	}
-	cpy[++j] = '\0';
-	/*strcpy(cpy, s1);*/
+	cpy = concat(s1, s2, cpy, n, len1, len2);
 	return (cpy);
 }
