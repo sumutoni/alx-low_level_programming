1641cf56119e2ab7cdb46008d48fd9a8f26eba49
stephanie <steumutoni@gmail.com>
2022-08-01 09:08:16 -0700

    function to allocate memory for an array of nmemb elements of size bytes each and returns a pointer to the allocated memory.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include<stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array of ints of nmemb elements each with size bytes
+ * @nmemb: number of elements of array
+ * @size: bytes to allocate to each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ar;
+	int i;
+
+	ar = malloc(nmemb  * size);
+	if (ar == NULL)
+		return (NULL);
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ar[i] = 0;
+	return (ar);
+}
