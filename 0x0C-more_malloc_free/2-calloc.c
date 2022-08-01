14246806402a993833443643c831f46c6dd83540
stephanie <steumutoni@gmail.com>
2022-08-01 09:37:28 -0700

    function to allocate memory for an array of nmemb elements of size bytes each and returns a pointer to the allocated memory.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,10 +15,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ar;
 	unsigned int i;
 
+	if (size == 0 || nmemb == 0)
+		return (NULL);
 	ar = malloc(nmemb  * size);
 	if (ar == NULL)
-		return (NULL);
-	if (size == 0 || nmemb == 0)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		ar[i] = 0;
