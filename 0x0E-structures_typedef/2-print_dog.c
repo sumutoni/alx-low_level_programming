67e7e11ac2daef7a6c9f0ed6e0db123c1b7b92d2
stephanie <steumutoni@gmail.com>
2022-08-02 17:06:33 -0700

    function to print information of a dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print information of a dog
+ * @d: dog variable
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("");
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %6f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
