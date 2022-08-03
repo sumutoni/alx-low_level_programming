ae62dc3d6cddab0e341f5db69a04ccb050c8e3f3
stephanie <steumutoni@gmail.com>
2022-08-02 19:05:46 -0700

    function to create a new dog variable and instatiate it

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,6 +4,37 @@
 #include <string.h>
 
 /**
+ * copy - function to instantiate dog variable
+ * @d: dog variable
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to dog variable
+ */
+dog_t *copy(dog_t *d, char *name, float age, char *owner)
+{
+	dog_t *dg;
+	int i, j;
+
+	i = 0;
+	j = 0;
+	dg->age = age;
+	while (name[i] != '\0')
+	{
+		dg->name[i] = name[i];
+		i++;
+	}
+	dg->name[i] = '\0';
+	while (owner[j] = '\0')
+	{
+		dg->owner[j] = owner [j];
+		j++;
+	}
+	dg->owner[j] = '\0';
+	return (dg);
+}
+/**
  * new_dog - creates a new variable of type dog
  * @name: name of dog
  * @age: age of dog
@@ -12,12 +43,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dg;
+	int len_n, len_o, i, j;
 
+	len_n = strlen(name);
+	len_o = strlen(owner);
 	dg = malloc(sizeof(struct dog));
-	if (dg == NULL)
+	if (dg == NULL || name == NULL || owner == NULL)
 		return (NULL);
-	strcpy(dg->name, name);
-	dg->age = age;
-	strcpy(dg->owner, owner);
+	dg->name = malloc(len_n + 1);
+	if (dg->name == NULL)
+		free_dog(dg);
+		return (NULL);
+	dg->owner = malloc(len_o + 1);
+	if (dg->owner == NULL)
+		free_dog(dg);
+		return (NULL);
+	dg = copy(dg, name, age, owner);
 	return (dg);
 }
