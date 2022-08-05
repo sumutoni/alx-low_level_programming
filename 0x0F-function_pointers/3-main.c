8949c86cbaccb84c50ba22c8bc8f599b551863ad
stephanie <steumutoni@gmail.com>
2022-08-05 14:03:21 -0700

    main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,13 +26,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (*(argv[2]) == '%' || *(argv[2]) == '/')
+	if ((*(argv[2]) == '%' || *(argv[2]) == '/') && *(argv[3]) == '0')
 	{
-		if (*(argv[3]) == '0')
-		{
-			printf("Error\n");
-			exit(100);
-		}
+		printf("Error\n");
+		exit(100);
 	}
 	result = f(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
