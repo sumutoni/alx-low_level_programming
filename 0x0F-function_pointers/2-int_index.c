3d73e9f52949eb19a70e15a38350781b8515a5de
stephanie <steumutoni@gmail.com>
2022-08-05 13:58:02 -0700

    Function to search for integer using function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,6 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, status;
 
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
