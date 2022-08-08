906175b2c848ff2d91af09ae24a7e3eb60c7fafc
stephanie <steumutoni@gmail.com>
2022-08-08 09:05:23 -0700

    added typedef variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,10 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+/**
+ * string -  typedef of *char
+ */
+typedef *char string;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
