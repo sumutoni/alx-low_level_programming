9bbbbc88372a718e3cb9a87cb015abb0017bcac7
stephanie <steumutoni@gmail.com>
2022-08-08 09:25:13 -0700

    added condition to check if separator is Nulland function to check if string is null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 
 /**
+ * check_null - check if string is null
+ * @s: string
+ */
+void check_null (char *s)
+{
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+/**
  * print_strings - print strings separated by a separator
  * @separator: string to be printed between strings
  * @n: number of strings passed to the function
@@ -12,17 +23,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (separator == NULL)
-		exit(0);
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
 		{
-			printf("%s%s", va_arg(list, char *), separator);
+			if (separator)
+			{
+				check_null(va_arg(list, char *));
+				printf("%s", separator);
+			}
+			else
+				check_null(va_arg(list, char *));
 		}
 		else
-			printf("%s", va_arg(list, char *));
+			check_null(va_arg(list, char *));
 	}
 	va_end(list);
 	printf("\n");
