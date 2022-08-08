0b151b625a0a59f260b7e2f45852d38ae2ecba61
stephanie <steumutoni@gmail.com>
2022-08-08 09:33:48 -0700

    added condition to check if separator is Nulland function to check if string is null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
-void print_integers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
