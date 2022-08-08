6cc11fa10c4dedadf97645bf3ac887bcb75d7cf1
stephanie <steumutoni@gmail.com>
2022-08-08 13:16:09 -0700

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,18 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+/**
+ * struct types - struct for types of arguments
+ * @pr: pointer to printing function
+ * @str: formatted string
+ * @sym: symbol
+ */
+typedef struct types
+{
+	void (*pr)(char *, va_list);
+	char *str;
+	char sym;
+}type;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
