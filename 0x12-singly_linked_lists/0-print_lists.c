83efd3b9bf7a8881b257a8f56b6a2093e4898319
stephanie <steumutoni@gmail.com>
2022-08-17 04:41:16 -0700

    Fixed string not printing issue

--- 0x12-singly_linked_lists/0-print_lists.c
+++ 0x12-singly_linked_lists/0-print_lists.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 
 /**
+ * print - print a number
+ * @i: number to print
+ */
+void print(unsigned int i)
+{
+	if (i / 10)
+		print(i / 10);
+	_putchar((i % 10) + '0');
+}
+/**
  * print_list - print elements of a list
  * @h: list of elements
  *
@@ -19,7 +29,7 @@ size_t print_list(const list_t *h)
 	while (next != NULL)
 	{
 		_putchar('[');
-		_putchar(next->len);
+		print(next->len);
 		_putchar(']');
 		_putchar(' ');
 		s = malloc(sizeof(char) * strlen(next->str));
@@ -28,11 +38,13 @@ size_t print_list(const list_t *h)
 			free(s);
 			break;
 		}
+		s = next->str;
 		while (*s != '\0')
 		{
 			_putchar(*s);
 			s++;
 		}
+		_putchar('\n');
 		nodes++;
 		next = next->next;
 	}
