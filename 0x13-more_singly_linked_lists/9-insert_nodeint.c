931aca9ce82db63b8041e80f8404565490cd7b11
stephanie <steumutoni@gmail.com>
2022-08-21 10:07:37 -0700

    checked if malloc fails

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,6 +43,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	index = 0;
 	node = *head;
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	while (node && index < idx)
 	{
