c697951b0ac25233bd59d71f4d3e36de33f7297c
stephanie <steumutoni@gmail.com>
2022-08-21 10:04:03 -0700

    function to insert node at specific index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+/**
+ * counte - counts number of nodes in list
+ * @h: head of list
+ *
+ * Return: number of node
+ */
+unsigned int counte(listint_t *h)
+{
+	unsigned int count;
+	listint_t *node;
+
+	node = h;
+	count = 0;
+	while (node)
+	{
+		count++;
+		node = node->next;
+	}
+	return (count);
+}
+/**
+ * insert_nodeint_at_index - insert a node at given index
+ * @head: pointer to head
+ * @idx: index where to insert
+ * @n: value
+ *
+ * Return: address of new inserted node or NULL if function fails
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int index, size;
+	listint_t *node;
+	listint_t *next;
+	listint_t *prev;
+	listint_t *new;
+
+	if (!head || *head == NULL)
+		return (NULL);
+	size = counte(*head);
+	if (size <= idx)
+		return (NULL);
+	index = 0;
+	node = *head;
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	while (node && index < idx)
+	{
+		prev = node;
+		node = node->next;
+		index++;
+	}
+	next = node;
+	prev->next = new;
+	new->next = next;
+	return (new);
+}
