e909a3dbc8b3be5fe0ab87518cb6995686791f1f
stephanie <steumutoni@gmail.com>
2022-08-22 04:49:14 -0700

    added condition to check if size of list is 1

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * get_size - gets number of nodes in list
+ * @h: head of list
+ *
+ * Return: size of list
+ */
+int get_size(listint_t *h)
+{
+	int size;
+	listint_t *node;
+
+	node = h;
+	size = 0;
+	while (node)
+	{
+		size++;
+		node = node->next;
+	}
+	return (size);
+}
+/**
  * pop_listint - deletes the head node
  * @head: head node
  *
@@ -9,10 +29,19 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *node;
-	int value;
+	int value, size;
 
 	if (!head || *head == NULL)
 		return (0);
+	size = get_size(*head);
+	if (size == 1)
+	{
+		value = (*head)->n;
+		*head = NULL;
+		free(*head);
+		free(head);
+		return (value);
+	}
 	node = (*head)->next;
 	value = (*head)->n;
 	*head = node;
