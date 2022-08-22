7fa6781dd688577dc0df3d8d02b8e048df08aa96
stephanie <steumutoni@gmail.com>
2022-08-22 05:05:03 -0700

    removed a condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,7 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	if (idx == 0 || *head == NULL)
+	if (idx == 0 || *head == NULL || !head)
 	{
 		new->next = node;
 		*head = new;
