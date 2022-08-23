48404627058a7909c9d3f69799ed973a57fa34c0
stephanie <steumutoni@gmail.com>
2022-08-23 11:11:57 -0700

    function to get bit at an index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,38 +1,6 @@
 #include "main.h"
 
 /**
- * count_binary - counts bits in binary representation of a number
- * @n: number to convert
- * @count: counts position
- * @idx: index of bit
- *
- * Return: value of bit at position idx
- */
-char count_binary(unsigned long int n, unsigned int count, unsigned int idx)
-{
-	char bit;
-
-	if (n == 0)
-	{
-		bit = '0';
-		return (bit);
-	}
-	if (n == 1)
-	{
-		bit = '1';
-		return (bit);
-	}
-	if (n >> 1)
-	{
-		count++;
-		bit = count_binary(n >> 1, count, idx);
-		if (count == idx)
-			return (bit);
-		bit = ((n & 1) == 0) ? '0' : '1';
-	}
-	return (bit);
-}
-/**
  * get_bit - get bit at index position
  * @n: number to convert to binary
  * @index: index of bit
@@ -41,12 +9,7 @@ char count_binary(unsigned long int n, unsigned int count, unsigned int idx)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
-	unsigned int count;
-
 	if (index >= 64)
 		return (-1);
-	count = -1;
-	bit = count_binary(n, count, index) - '0';
-	return (bit);
+	return ((n >> index) & 1);
 }
