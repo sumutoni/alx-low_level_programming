c6125e6acc5771f15a7f32117fd339755e9b6e5b
stephanie <steumutoni@gmail.com>
2022-08-23 08:57:33 -0700

    function to return bit value at a position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * count_binary - counts bits in binary representation of a number
+ * @n: number to convert
+ * @count: counts position
+ * @idx: index of bit
+ *
+ * Return: value of bit at position idx
+ */
+char count_binary(unsigned long int n, int count, unsigned int idx)
+{
+	char bit;
+
+	if (n == 0)
+	{
+		bit = '0';
+		return (bit);
+	}
+	if (n == 1)
+	{
+		bit = '1';
+		return (bit);
+	}
+	if (n >> 1)
+	{
+		count++;
+		bit = count_binary(n >> 1, count, idx);
+		if (count == idx)
+			return (bit);
+		bit = ((n & 1) == 0) ? '0' : '1';
+	}
+	return (bit);
+}
+/**
+ * get_bit: get bit at index position
+ * @n: number to convert to binary
+ * @index: index of bit
+ *
+ * Return: value of bit, -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int idx;
+	int bit, count;
+
+	count = -1;
+	bit = count_binary(n, count, index) - '0';
+	return (bit);	
+}
