314e254cda8120bae516298105a0c76c246d477d
stephanie <steumutoni@gmail.com>
2022-08-25 11:27:47 -0700

    new changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,17 +16,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	file_d = open(filename, O_RDONLY);
-	if (file_d == -1)
+	if (file_d < 0)
 		return (0);
-	file = malloc(sizeof(char));
+	file = malloc(sizeof(char) * letters);
 	if (!file)
 		return (0);
 	bytes_r = read(file_d, file, letters);
-	if (bytes_r == -1)
+	if (bytes_r < 0)
+	{
+		free(file);
 		return (0);
-	if (bytes_r != 0)
-		bytes_w = write(STDOUT_FILENO, file, bytes_r);
-	if (bytes_w != letters || close(file_d) == -1)
+	}
+
+	file[byte_r] = '\0';
+
+	close(file_d);
+
+	bytes_w = write(STDOUT_FILENO, file, bytes_r);
+	if (bytes_w < 0)
+	{
+		free(file);
 		return (0);
+	}
+	free(file);
 	return (bytes_w);
 }
