3c180bed6a4260d3d90bbe7e58a24c6785ce93d7
stephanie <steumutoni@gmail.com>
2022-08-25 11:17:56 -0700

    new changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
@@ -11,5 +12,6 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 #endif
