18a01f4501ceb3910fb35162c800856fec6fd265
stephanie <steumutoni@gmail.com>
2022-08-25 11:50:52 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	size_t bytes;
+	int file_d, s = 0;
+	size_t bytes_r;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		if (file_d == -1)
-			return (-1);
-		return (1);
-	}
+
 	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file_d == -1)
+	if (file_d < 0)
 		return (-1);
-	bytes = write(file_d, text_content, strlen(text_content));
-	if (bytes == -1 || close(file_d) == -1)
+
+	if (text_content)
+	{
+		while (text_content[s])
+			s++;
+	bytes = write(file_d, text_content, s);
+	if (bytes != s)
 		return (-1);
+	}
+
+	close(file_d);
 	return (1);
 }
