11ee7c8f1b997f48c3f8adadaf457e27fcef6b46
sumutoni <steumutoni@gmail.com>
2022-09-29 11:08:06 -0700

    function to return nth node of list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * count - returns number of nodes in list
+ * @head: head of list
+ *
+ * Return: number of nodes
+ */
+unsigned int count(dlistint_t *head)
+{
+	unsigned int size = 0;
+	dlistint_t *curr;
+
+	curr = head;
+	while (curr)
+	{
+		size++;
+		curr = curr->next;
+	}
+	return (size);
+}
+/**
+ * get_dnodeint_at_index - returns nth node of list
+ * @head: head of list
+ * index: index of node
+ *
+ * Return: node at nth index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint *curr;
+	unsigned int size, count = 0;
+
+	if (!head)
+		return (NULL);
+	size = count(head);
+	if (index >= size)
+		return (NULL);
+	curr = head;
+	while (curr)
+	{
+		if (count == index)
+			break;
+		curr = curr->next;
+		count++;
+	}
+	return (curr);
+}
